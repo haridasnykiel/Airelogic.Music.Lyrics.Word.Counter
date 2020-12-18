8e0d18a36760412dff8a8c2cbe9abfb0715b7e5a
Haridas Nykiel <Haridas.Nykiel@jet2.com>
2020-12-18 14:17:45 +0000

    .

---
+++ Models/Artist.cs
@@ -0,0 +1,8 @@
+namespace Music.Lyrics.Word.Counter.Models
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Services/MusicApiClient.cs
+++ Services/MusicApiClient.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
 using System.Threading.Tasks;
 using MetaBrainz.MusicBrainz;
+using Music.Lyrics.Word.Counter.Models;
 
 namespace Music.Lyrics.Word.Counter.Services
 {
@@ -15,13 +19,17 @@ namespace Music.Lyrics.Word.Counter.Services
             {
                 BaseAddress = new Uri("https://musicbrainz.org/ws/2/")
             };
+            client.DefaultRequestHeaders
+                .Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        public async Task<string> SearchArtistDataAsync(string artistName)
+        public async Task<Artist> SearchArtistAsync(string artistName)
         {
             var artistData = await client.GetAsync("artist/cc197bad-dc9c-440d-a5b5-d52ba2e14234");
-            var data = await artistData.Content.ReadAsStringAsync();
-            return data;
+            var stream = await artistData.Content.ReadAsStreamAsync();
+            var artist = await JsonSerializer.DeserializeAsync<Artist>(stream);
+            return artist;
         }
     }
 }
